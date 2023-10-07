11dd0db77072ee46ce7cb28e5b62b4d594461dc9
SANDHH <sisa1901@student.miun.se>
2023-10-07 10:53:27 +0200

    d

--- Simons code/Servo_On.py
+++ Simons code/Servo_On.py
@@ -2,12 +2,18 @@ import socket
 HOST = '127.0.0.1'
 PORT = 10002
 
-
+import socket
+# Set a timeout value (in seconds)
+TIMEOUT = 5
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST,PORT))
+    s.settimeout(TIMEOUT)  # Set the timeout
+    s.connect((HOST, PORT))
     s.sendall(b'20')
-    data = s.recv(1024)
-print('Received', repr(data)) #repr returns printable data.
+    try:
+        data = s.recv(1024)
+        print('Received', repr(data))
+    except socket.timeout:
+        print('No data received after', TIMEOUT, 'seconds')
 
 
 
