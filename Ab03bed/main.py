9e9f381e5804a14f63c28311e7b6b975d4ab12c3
Ab03beed <abdaher2000@gmail.com>
2023-10-10 20:19:03 +0200

    Added server.py and some other changes
    .

--- Ab03bed/main.py
+++ Ab03bed/main.py
@@ -7,26 +7,39 @@ import os
 
 def main():
     #Object of RobotController class
-    #r1 = RobotController("127.0.0.1", 10002, "raspiry", 242)
+    r1 = RobotController("127.0.0.1", 10002, "127.0.0.1", 12345)
     #Object of SpeechToText class
     sp = SpeechToText()
+    #Object of GPT_API class
     gpt = GPT_API()
-    
-
-    
   
     while True:
-        
-        res = r1.sendTo(r1.robotSoc,  "200,0,645")
-        print("res: ", res)
-        time.sleep(1)
-        res = r1.sendTo(r1.robotSoc,  "300,0,645")
-        print("res: ", res)
-        
-    task = sp.talk()
-    print(task)
+        task = sp.talk()
+        if task != "none":
+            gptResponse = gpt.ask(task)
 
-    print(gpt.ask(task))
+            print(gptResponse)
+
+            for line in gptResponse.split("\n"):
+                if "go_to_location" in line:
+                    coords = [int(coord) for coord in line.split("(")[1].split(")")[0].split(",")]
+                    command = f"{coords[0]},{coords[1]},{coords[2]}"
+                    res = r1.sendTo(r1.robotSoc, command)
+                    print(res)
+                elif "grab()" in line:
+                    res = r1.sendTo(r1.raspSoc, "GRAB")
+                    print(res)
+                elif "release()" in line:
+                    res = r1.sendTo(r1.raspSoc, "RELEASE")
+                    print(res)
+                    
+
+            
+        
+        
+        
+        
+    
 
 
 main()
