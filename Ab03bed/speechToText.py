71fbcf950f86811bca1ebefde63ce951a8280088
Ab03beed <abdaher2000@gmail.com>
2023-10-17 13:57:13 +0200

    .
    .

--- Ab03bed/speechToText.py
+++ Ab03bed/speechToText.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import keyboard
 
 class SpeechToText:
 
@@ -8,8 +9,11 @@ class SpeechToText:
         
     
     def talk(self):
+        print("\nPress V to talk ...")
+        keyboard.wait('v')
+        
         with sr.Microphone() as mic:
-            print("I'm listening...")
+            print("Which box should I move?")
             audio = self.recognizer.listen(mic)
 
         return self._convertAudioToText(audio)
@@ -19,12 +23,14 @@ class SpeechToText:
     def _convertAudioToText(self, audio):
         try:
             text = self.recognizer.recognize_google(audio).lower()
-            print("I've heard: ", text) 
+            print("I've heard: ", text, '\n') 
             return text
         except sr.UnknownValueError:
             #Handle unrecognized audio
+            print("Couldn't undersatnd the what you said")
             return "none"
         except sr.RequestError:
             # Handle request errors
             print("Could not request results from the speech recognition service.")
-            return "none"        
+            return "none"
+                
